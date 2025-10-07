4d9091fbdafc75f28bbeb7b4987fa9ed0292ea31
J. Meuphilim <meuphilim@gmail.com>
2025-10-07 18:55:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import { AuthProvider } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { Layout } from './components/Layout/Layout';
 import { Dashboard } from './pages/Dashboard';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import { Schedule } from './pages/Schedule';
 import { Packages } from './pages/Packages';
 import { Settings } from './pages/Settings';
@@ -14,6 +14,7 @@ import { UserManagement } from './pages/UserManagement';
 
 function App() {
   return (
+    <ErrorBoundary>
     <AuthProvider>
       <DataProvider>
         <Router>
