8e384413fb1e7bb74dca4cfe9621353e8c1543d5
J. Meuphilim <meuphilim@gmail.com>
2025-10-07 00:32:51 -0400

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" /> 
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_SESSION_TIMEOUT: string
+  readonly VITE_APP_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare const __APP_VERSION__: string
