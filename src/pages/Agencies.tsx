97c9d1818aaf59fe22c11c6df204249a59d1a98f
J. Meuphilim <meuphilim@gmail.com>
2025-10-06 23:29:54 -0400

    Updated Agencies.tsx

--- src/pages/Agencies.tsx
+++ src/pages/Agencies.tsx
@@ -196,7 +196,7 @@ export const Agencies: React.FC = () => {
                       <PencilIcon className="h-4 w-4" />
                     </button>
                     <button
-                      onClick={() => handleDelete(agency.id)}
+                      // onClick={() => handleDelete(agency.id)}
                       onClick={() => handleDelete(agency.id, agency.name)}
                       className="text-red-600 hover:text-red-900 transition-colors duration-200"
                     >
