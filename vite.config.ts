ec1932939266c06cc63403b1ac55b4acb82e2bf2
J. Meuphilim <meuphilim@gmail.com>
2025-10-11 13:09:38 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/ 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    chunkSizeWarningLimit: 1500, // aumenta o limite para evitar avisos falsos
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // separa dependências grandes em chunks específicos
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            return 'vendor'; // fallback geral para outros pacotes
+          }
+        },
+      },
+    },
+  },
 });
+
