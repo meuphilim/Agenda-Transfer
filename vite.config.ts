0d45fc944be217911187a88d7c8290255b575b24
bonitoecoexpedicoes <meuphilim@gmail.com>
2025-10-12 18:22:45 -0400

    Atualização lista de arquivos

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/ 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }), // abre automaticamente no navegador após build
+  ],
   base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
